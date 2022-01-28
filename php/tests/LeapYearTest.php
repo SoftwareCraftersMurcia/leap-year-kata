e378a6751b05aa70c2622d409ba122ac99b251c5
Miguel Manzano García <mmanzano@gmail.com>
2022-01-28 18:57:46 +0100

    Move common code to SetUp method

--- php/tests/LeapYearTest.php
+++ php/tests/LeapYearTest.php
@@ -7,19 +7,28 @@ use PHPUnit\Framework\TestCase;
 
 class LeapYearTest extends TestCase
 {
+    private LeapYear $leapYear;
+
+    protected function setUp(): void
+    {
+        $this->leapYear = new LeapYear();
+    }
+
     /** @test */
     public function given_2_is_not_leap_year(): void
     {
-        $leapYear = new LeapYear();
-
-        self::assertFalse($leapYear->isLeapYear(2));
+        self::assertFalse($this->leapYear->isLeapYear(2));
     }
 
     /** @test */
     public function given_4_is_not_leap_year(): void
     {
-        $leapYear = new LeapYear();
+        self::assertTrue($this->leapYear->isLeapYear(4));
+    }
 
-        self::assertTrue($leapYear->isLeapYear(4));
+    /** @test */
+    public function given_8_is_not_leap_year(): void
+    {
+        self::assertTrue($this->leapYear->isLeapYear(8));
     }
 }
