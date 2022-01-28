f989d697fa6c15ca7ad0c829f1bbd2b44435fc40
Miguel Manzano García <mmanzano@gmail.com>
2022-01-28 19:10:03 +0100

    given_a_number_divisible_by_100_but_not_by_400_is_not_a_leap_year

--- php/src/LeapYear.php
+++ php/src/LeapYear.php
@@ -6,6 +6,15 @@ class LeapYear
 {
     public function isLeapYear(int $year): bool
     {
+        if ($this->isDivisibleBy100ButNotBy400($year)) {
+            return false;
+        }
+
         return $year % 4 === 0;
+    }
+
+    private function isDivisibleBy100ButNotBy400(int $year): bool
+    {
+        return $year % 100 === 0 && $year % 400 !== 0;
     }
 }
--- php/tests/LeapYearTest.php
+++ php/tests/LeapYearTest.php
@@ -14,21 +14,30 @@ class LeapYearTest extends TestCase
         $this->leapYear = new LeapYear();
     }
 
-    /** @test */
+    /** @test **/
     public function given_2_is_not_leap_year(): void
     {
         self::assertFalse($this->leapYear->isLeapYear(2));
     }
 
-    /** @test */
+    /** @test **/
     public function given_4_is_not_leap_year(): void
     {
         self::assertTrue($this->leapYear->isLeapYear(4));
     }
 
-    /** @test */
+    /** @test **/
     public function given_8_is_not_leap_year(): void
     {
         self::assertTrue($this->leapYear->isLeapYear(8));
+    }
+
+    /** @test **/
+    public function given_a_number_divisible_by_100_but_not_by_400_is_not_a_leap_year(): void
+    {
+        self::assertFalse($this->leapYear->isLeapYear(600));
+        self::assertFalse($this->leapYear->isLeapYear(1000));
+        self::assertFalse($this->leapYear->isLeapYear(1400));
+        self::assertFalse($this->leapYear->isLeapYear(1800));
     }
 }
