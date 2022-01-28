e7031ccaf33e8cb75a861d90e13db31b408c1091
Luis Rovirosa Mairlot <luis@codium.team>
2022-01-28 18:28:47 +0100

    [kotlin] initial version maybe broken

---
+++ kotlin/src/test/java/LeapYearTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class LeapYearTest {
+
+    @Test
+    fun `xxx`(number: Int) {
+        val validator = LeapYearValidator()
+
+        assertFalse { false }
+    }
+}
