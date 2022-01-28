eeb09893f16248442572d19d149c257077909441
Chemaclass <chemaclass@outlook.es>
2022-01-28 18:43:32 +0100

    given_2_is_not_leap_year

---
+++ php/tests/LeapYearTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace KataTests;
+
+use Kata\LeapYear;
+use PHPUnit\Framework\TestCase;
+
+class LeapYearTest extends TestCase
+{
+    /** @test */
+    public function given_2_is_not_leap_year(): void
+    {
+        $leapYear = new LeapYear();
+
+        self::assertFalse($leapYear->isLeapYear(2));
+    }
+}
