2158ef2772620b9cca6f43ff9fe8760f4ad7821b
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-01-30 23:22:48 +0100

    remove the initialization from each test to a setup method

--- kotlin/src/test/java/LeapYearTest.kt
+++ kotlin/src/test/java/LeapYearTest.kt
@@ -1,13 +1,19 @@
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class LeapYearTest {
 
+    private lateinit var validator: LeapYearValidator;
+
+    @BeforeEach
+    fun setup() {
+        validator = LeapYearValidator()
+    }
+    ;
     @Test
     fun `should return true when year is 4`() {
-        val validator = LeapYearValidator()
-
         val isLeapYear: Boolean = validator.isLeapYear(4)
 
         assertTrue { isLeapYear }
@@ -15,8 +21,6 @@ class LeapYearTest {
 
     @Test
     fun `should return false when year is 3`() {
-        val validator = LeapYearValidator()
-
         val isLeapYear: Boolean = validator.isLeapYear(3)
 
         assertFalse { isLeapYear }
@@ -24,8 +28,6 @@ class LeapYearTest {
 
     @Test
     fun `should return false when year is 100`() {
-        val validator = LeapYearValidator()
-
         val isLeapYear: Boolean = validator.isLeapYear(100)
 
         assertFalse { isLeapYear }
@@ -33,8 +35,6 @@ class LeapYearTest {
 
     @Test
     fun `should return true when year is 400`() {
-        val validator = LeapYearValidator()
-
         val isLeapYear: Boolean = validator.isLeapYear(400)
 
         assertTrue { isLeapYear }
